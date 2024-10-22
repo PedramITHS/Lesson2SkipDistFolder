3db3600873f6654c3391c4a7322b20a02157cb57
Pedram <pedram.hejazi.kenari@iths.se>
2024-10-22 15:04:46 +0200

    added cors npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("http://localhost:3000")
+      .then((resp) => resp.json())
+      .then((data) => console.log(data));
+  }, []);
 
   return (
     <>
