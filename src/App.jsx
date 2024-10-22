08a69b8f102dfd58dd622c4972699ec4d6bd5b95
Pedram <pedram.hejazi.kenari@iths.se>
2024-10-22 11:56:40 +0200

    Added H4, color lime

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
         </p>
       </div>
       <h3 style={{ color: "red" }}>A new change to the file, added H3!</h3>
+      <h4 style={{ color: "lime" }}>A new change to the file, added H4!</h4>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
